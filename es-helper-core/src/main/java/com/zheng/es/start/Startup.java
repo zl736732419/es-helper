53b608537cb4a3b29eb4ec85f80f3ddbc89b91f1
zhenglian <zhenglian@globalegrow.com>
2019-04-13 17:46:04 +0800

    修改项目依赖

---
+++ es-helper-core/src/main/java/com/zheng/es/start/Startup.java
@@ -0,0 +1,20 @@
+package com.zheng.es.start;
+
+/**
+ * <pre>
+ *
+ *  File:
+ *
+ *  Copyright (c) 2016, globalegrow.com All Rights Reserved.
+ *
+ *  Description:
+ *  服务启动
+ *
+ *  Revision History
+ *  Date,					Who,					What;
+ *  2019年04月13日			zhenglian			    Initial.
+ *
+ * </pre>
+ */
+public class Startup {
+}
